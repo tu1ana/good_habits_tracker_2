e7eb9798f96eaf9748c9391b96246db7a3e78ebb
Tuyana Chirninova <hikkonki@gmail.com>
2023-12-05 22:17:56 +0300

    habits whole app

---
+++ habits/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from habits.models import Habit
+from habits.validators import *
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    """ Сериализатор модели Habit """
+    class Meta:
+        model = Habit
+        fields = '__all__'
+        validators = [
+            validate_related_habit_and_reward,
+            validate_duration,
+            validate_related_habit,
+            validate_nice_habit,
+            validate_frequency
+        ]
